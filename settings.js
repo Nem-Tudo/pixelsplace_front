464cf23173ca104e822833af3520afc0efeac6fc
Nem Tudo <gf98289@gmail.com>
2025-06-21 17:30:41 -0300

    feat: dev options

--- settings.js
+++ settings.js
@@ -1,3 +1,9 @@
+// module.exports = {
+//     apiURL: "http://localhost:3032",
+//     socketURL: "ws://localhost:3032",
+//     avatarURL: (userid, avatar) => `https://cdn.discordapp.com/avatars/${userid}/${avatar}.webp?size=512`
+// }
+
 module.exports = {
     apiURL: "https://apipixelsplace.nemtudo.me",
     socketURL: "wss://apipixelsplace.nemtudo.me",
