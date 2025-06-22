7d086f57322542f4373bd13adbe28ee34a1b49ca
Renato <renato.augusto.almeida.santos@gmail.com>
2025-06-22 19:27:38 -0300

    Change: Premium Warning conversion to Premium Popup

--- components/PremiumWarning.jsx
+++ components/PremiumWarning.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from "react";
 import { MdClose } from "react-icons/md";
 import Link from "next/link";
 import PremiumButton from "@/components/PremiumButton";
+import PremiumPopup from "@/components/PremiumPopup";
 import styles from "./PremiumWarning.module.css"; // ou o mesmo 'commandbat.module.css'
 
 export default function PremiumWarning({ onClose }) {
@@ -20,7 +21,7 @@ export default function PremiumWarning({ onClose }) {
     }, [onClose]);
 
     return (
-        <div ref={divRef} className={styles.div}>
+        /*<div ref={divRef} className={styles.div}>
             <div
                 style={{
                     position: "fixed",
@@ -37,6 +38,7 @@ export default function PremiumWarning({ onClose }) {
             <PremiumButton setClass={styles.button} as={Link} redirect={true} href="/premium">
                 <span className={styles.span}>Compre o Premium aqui!</span>
             </PremiumButton>
-        </div>
+        </div>*/
+        <PremiumPopup onClose={() => onClose()}></PremiumPopup>
     );
 }
