e18b898bb5602b2a01d34cad4781f4d6997aca5b
Renato <renato.augusto.almeida.santos@gmail.com>
2025-06-25 17:31:27 -0300

    Change: /place's share icon now uses PixelArtIcon

--- pages/place.jsx
+++ pages/place.jsx
@@ -723,7 +723,7 @@ export default function Place() {
                     copyText(link);
                     alert(`Link copiado para a área de transferência! (x: ${selectedPixel.x}, y: ${selectedPixel.y}, scale: ${Math.round(transform.current.scale)})`);
                   }}>
-                    <FaShare />
+                    <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M14 5h-2v4H6v2H4v6h2v-2h6v4h2v-2h2v-2h2v-2h2v-2h-2V9h-2V7h-2V5z" fill="currentColor"/> </svg>
                   </div>
                 </Tippy>
               </div>
