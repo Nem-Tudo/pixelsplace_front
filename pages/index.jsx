a7ea86acf5b42a5c6b4832a721a1311a93e2fd94
Renato <renato.augusto.almeida.santos@gmail.com>
2025-06-25 17:25:39 -0300

    Change: Index Buttons now have Icons

--- pages/index.jsx
+++ pages/index.jsx
@@ -35,11 +35,14 @@ export default function Home() {
               Começar
             </CustomButton>
             {
-              checkFlags(loggedUser?.flags, "TIMETRAVEL_VIEW") && <PremiumButton as={Link} href={"/timetravel"}>Time Travel</PremiumButton>
+              checkFlags(loggedUser?.flags, "TIMETRAVEL_VIEW") && <PremiumButton as={Link} href={"/timetravel"}><svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M18 2H6v6h2v2h2v4H8v2H6v6h12v-6h-2v-2h-2v-4h2V8h2V2zm-2 6h-2v2h-4V8H8V4h8v4zm-2 6v2h2v4H8v-4h2v-2h4z" fill="currentColor"/> </svg>Time Travel</PremiumButton>
             }
-            <CustomButton label={'Servidores'} href={"/partners"} hierarchy={2} />
+            <CustomButton href={"/partners"} hierarchy={2}>
+              <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M3 3h18v18H3V3zm2 2v6h14V5H5zm14 8H5v6h14v-6zM7 7h2v2H7V7zm2 8H7v2h2v-2z" fill="currentColor"/> </svg>
+              Servidores
+            </CustomButton>
             {
-              checkFlags(loggedUser?.flags, "ADMIN_VIEWPAGE") && <CustomButton label={'⚙ Admin'} href={"/admin"} hierarchy={3}/>
+              checkFlags(loggedUser?.flags, "ADMIN_VIEWPAGE") && <CustomButton href={"/admin"} hierarchy={3}><svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M4 4h18v7h-4v5H4V4zm14 5h2V6h-2v3zm-2-3H6v8h10V6zm3 14H3v-2h16v2z" fill="currentColor"/> </svg>Admin</CustomButton>
             }
             {
               checkFlags(loggedUser?.flags, "BUILD_OVERRIDE_VIEW") && <BuildSwitcher />
