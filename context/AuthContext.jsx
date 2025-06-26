9f72b06912735ea65fb28767a95a85a0047853fa
Nem Tudo <gf98289@gmail.com>
2025-06-26 17:29:47 -0300

    increase random pixel range

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -69,8 +69,8 @@ export function AuthProvider({ children }) {
                     "Authorization": utoken
                 },
                 body: JSON.stringify({
-                    x: randomIntFromInterval(100, 138),
-                    y: randomIntFromInterval(59, 68),
+                    x: randomIntFromInterval(2, 138),
+                    y: randomIntFromInterval(75, 97),
                     c: randomIntFromInterval(1, 16777214)
                 })
             })
