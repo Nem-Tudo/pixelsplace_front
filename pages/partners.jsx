68c6b38506fb1f5389466a0d05b1d65cb50dc86e
Nem Tudo <gf98289@gmail.com>
2025-06-26 15:43:15 -0300

    refator: get guilds data from api

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import styles from "./partners.module.css";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BsArrowLeft, BsStar, BsStarHalf, BsFillStarFill } from "react-icons/bs";
 import { MainLayout } from "@/layout/MainLayout";
 import checkFlags from "@/src/checkFlags";
@@ -12,17 +12,39 @@ import settings from "@/settings";
 // Hover: bs BsStarHalf
 // Selecionado: bs BsFillStarFill
 
+export async function getServerSideProps() {
+    try {
+        const res = await fetch(`${settings.apiURL}/guilds`, {
+            headers: { 'Content-Type': 'application/json' },
+        });
+        const data = await res.json();
+        if (!res.ok) return { props: { error: true, errormessage: data.message } };
+        return { props: { guilds: data } };
+    } catch (e) {
+        return { props: { error: true, errormessage: e.message } };
+    }
+}
 
-export default function Partners() {
+export default function Partners({ guilds, error, errormessage }) {
 
     const { loggedUser } = useAuth()
     const { language } = useLanguage();
 
-    const guilds = []
-
     const [hovered, setHovered] = useState(null);
     const [userServer, setUserServer] = useState(null);
 
+    useEffect(() => {
+        setUserServer(loggedUser?.settings.selected_guild)
+    }, [loggedUser])
+
+    if (error) return (
+        <>
+            <div>
+                <h1>Erro ao carregar servidores</h1>
+                <span>Mensagem: {errormessage}</span>
+            </div>
+        </>
+    )
 
     return (<>
 
@@ -39,7 +61,7 @@ export default function Partners() {
                 <div className={styles.servers}>
                     {!checkFlags(loggedUser?.flags, "VIEW_PARTNERS") && <span>{language.getString("PAGES.PARTNERS.SECRET")}</span>}
                     {checkFlags(loggedUser?.flags, "VIEW_PARTNERS") && guilds.slice(0, 30).map((guild, index) => (
-                        <div key={index} className={styles.guildCard} style={userServer === guild.id ? { background: "linear-gradient(rgb(255 255 0 / 10%), rgb(255 255 0 / 10%)), #141c2f" } : {}}>
+                        <div key={index} className={styles.guildCard} style={userServer === guild.id ? { background: "linear-gradient(rgb(0 255 81 / 10%), rgb(10 255 115 / 16%)), rgb(34 38 35)" } : {}}>
                             <img
                                 className={styles.guildIcon}
                                 src={settings.guildIconURL(guild.id, guild.icon)}
@@ -47,7 +69,7 @@ export default function Partners() {
                             />
                             <div className={styles.guildInfo}>
                                 <h2 className={styles.guildName} translate="no">{guild.name}</h2>
-                                <a className={styles.guildLink} href="https://discord.gg/nemtudo" target="_blank" rel="norreferer">{language.getString("PAGES.PARTNERS.JOIN")}</a>
+                                <a className={styles.guildLink} href={guild.invite} target="_blank" rel="norreferer">{language.getString("PAGES.PARTNERS.JOIN")}</a>
                                 <div
                                     onMouseEnter={() => setHovered(guild.id)}
                                     onMouseLeave={() => setHovered(null)}
