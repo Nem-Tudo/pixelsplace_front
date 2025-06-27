036523489b32fd999a3ac1082a321ac8f7040216
Renato <renato.augusto.almeida.santos@gmail.com>
2025-06-27 18:13:31 -0300

    Feat: New color functions: hexToHsl and hexToCssHsl

--- src/colorFunctions.js
+++ src/colorFunctions.js
@@ -1,4 +1,3 @@
-// Função para escurecer cores hexadecimais
 export function darkenHex(hex, amount = 20) {
   // Remove o # se presente
   hex = hex.replace('#', '');
@@ -15,12 +14,12 @@ export function darkenHex(hex, amount = 20) {
 
   // Converte de volta para hex
   return `#${newR.toString(16).padStart(2, '0')}${newG.toString(16).padStart(2, '0')}${newB.toString(16).padStart(2, '0')}`;
-};
+}
 
-// Vaca funções
 export function hexToNumber(hex) {
   return parseInt(hex.replace("#", ""), 16);
 }
+
 export function numberToHex(num = 0) {
   return "#" + num.toString(16).padStart(6, "0");
 }
@@ -43,4 +42,61 @@ export function getBrightness(hexColor) {
   const brightness = (0.2126 * r + 0.7152 * g + 0.0722 * b) / 255;
 
   return brightness;
+}
+
+export function hexToHsl(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    r = parseInt(result[1], 16);
+    g = parseInt(result[2], 16);
+    b = parseInt(result[3], 16);
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+    if(max == min){
+      h = s = 0; // achromatic
+    }else{
+      var d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch(max){
+        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+        case g: h = (b - r) / d + 2; break;
+        case b: h = (r - g) / d + 4; break;
+      }
+      h /= 6;
+    }
+  return {
+    h, s, l
+  };
+}
+
+export function hexToCssHsl(hex, valuesOnly = false) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  var r = parseInt(result[1], 16);
+  var g = parseInt(result[2], 16);
+  var b = parseInt(result[3], 16);
+  var cssString = '';
+  r /= 255, g /= 255, b /= 255;
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch(max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+    h /= 6;
+  }
+  
+  h = Math.round(h * 360);
+  s = Math.round(s * 100);
+  l = Math.round(l * 100);
+  
+  cssString = h + ',' + s + '%,' + l + '%';
+  cssString = !valuesOnly ? 'hsl(' + cssString + ')' : cssString;
+  
+  return cssString;
 }
