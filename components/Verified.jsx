08a13e6207bc1de8ea6b5ac69d06ed4c7be50e35
Renato <renato.augusto.almeida.santos@gmail.com>
2025-06-28 22:36:51 -0300

    Feat: Tippy on verified component and corrections

--- components/Verified.jsx
+++ components/Verified.jsx
@@ -1,6 +1,14 @@
 import styles from "./Verified.module.css"
 import PixelIcon from "@/components/PixelIcon"
+import Tippy from "@tippyjs/react";
+import { useLanguage } from '@/context/LanguageContext';
 
 export default function Verified({ verified }) {
-    return verified ? <PixelIcon codename={'check'} className={styles.verified} /> : <></>
+    const { language } = useLanguage();
+
+    return verified ? 
+        <Tippy content={language.getString("COMMON.VERIFIED")} placement="top">
+            <PixelIcon codename={'check'} className={styles.verified} /> 
+        </Tippy>
+    : <></>
 }
