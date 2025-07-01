eecba5b97b8dca0d68d12f6d36f4bdf859978dfb
Bruno Ramos <37979213+Kuriel23@users.noreply.github.com>
2025-07-01 23:15:12 +0100

    fix: typeof window

--- src/SoundEngine.js
+++ src/SoundEngine.js
@@ -1,22 +1,21 @@
-import { useEffect, useState } from "react";
-
 const SoundEngine = (function () {
   const audioRefs = {};
 
   function load(files = []) {
+    if (typeof window === 'undefined') return; // SSR protection
+    
     files.forEach((file) => {
       if (!audioRefs[file]) {
-        const [audio, setAudio] = useState(null)
-        useEffect(() => {
-          setAudio(new Audio(`/sfx/${file}`))
-          audio.load();
-          audioRefs[file] = audio;
-        }, [])
+        const audio = new Audio(`/sfx/${file}`);
+        audio.load();
+        audioRefs[file] = audio;
       }
     });
   }
 
   function play(file) {
+    if (typeof window === 'undefined') return; // SSR protection
+    
     const audio = audioRefs[file];
     if (audio) {
       audio.currentTime = 0;
