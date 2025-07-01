366ae11053fd9825da66ce63e6bd3fc51d1264a3
command-bat <tiagolaure@gmail.com>
2025-07-01 17:05:52 -0300

    estou testando algo.

--- layout/MainLayout.jsx
+++ layout/MainLayout.jsx
@@ -85,8 +85,17 @@ export function MainLayout({ children }) {
     return (
         <>
             <Head>
-                <title>{`PixelsPlace`}</title>
+                <title>PixelsPlace</title>
+                <meta name="description" content={language.getString("PAGES.PLACE.META_DESCRIPTION")} />
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
                 <meta name="theme-color" content="#80bbff" />
+                <link rel="icon" href="/favicon.ico" />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="PixelsPlace.bat" />
+                <meta property="og:description" content="Coloque um pixel." />
+                <meta property="og:image" content="https://commandbat.com.br/imagens/Icon.jpg" />
+                <meta property="og:url" content="https://google.com" />
             </Head>
             <Header />
             <main className={styles.main}>
--- pages/place.jsx
+++ pages/place.jsx
@@ -677,13 +677,7 @@ export default function Place() {
 
   return (
     <>
-      <Head>
-        <title>PixelsPlace</title>
-        <meta name="description" content={language.getString("PAGES.PLACE.META_DESCRIPTION")} />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
-        <meta name="theme-color" content="#80bbff" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+
       <MainLayout>
         <section className={styles.overlayGui}>
           <div className={styles.top}>
