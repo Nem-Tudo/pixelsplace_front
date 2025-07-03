4159c76ab76279c81b2821a80422a991808d7711
Nem Tudo <gf98289@gmail.com>
2025-07-03 00:28:21 -0300

    change: credits easter egg

--- pages/credits.jsx
+++ pages/credits.jsx
@@ -3,14 +3,92 @@ import Link from "next/link";
 import { MainLayout } from "@/layout/MainLayout";
 import CustomHead from "@/components/CustomHead";
 import { useLanguage } from "@/context/LanguageContext";
+import { useEffect, useState } from "react";
 
 export default function Credits() {
 
   const { language } = useLanguage();
 
+
+  const CREDITS_NORMAL = [
+
+    {
+      name: 'Nem Tudo',
+      id: '612651439701098558',
+      role: "CEO"
+    },
+    {
+      name: 'Commandbat',
+      id: '385478022670843904',
+      role: "Developer frontend"
+    },
+    {
+      name: 'Renato',
+      id: '427257953503019017',
+      role: "Designer"
+    },
+    {
+      name: 'Kuriel',
+      id: '354233941550694400',
+      role: "Developer backend"
+    }
+
+  ];
+
+  const CREDITS_EASTERGG = [
+
+    {
+      name: 'Nem Tudo',
+      id: '612651439701098558',
+      role: language.getString("PAGES.CREDITS.CHIEF_COW")
+    },
+    {
+      name: 'Commandbat',
+      id: '385478022670843904',
+      role: language.getString("PAGES.CREDITS.CHIEF_DRAGGABLE")
+    },
+    {
+      name: 'Renato',
+      id: '427257953503019017',
+      role: language.getString("PAGES.CREDITS.GOAT")
+    },
+    {
+      name: 'Kuriel',
+      id: '354233941550694400',
+      role: language.getString("PAGES.CREDITS.CHIEF_EVIL_LINUX")
+    }
+
+  ];
+
+  const [credits, setCredits] = useState(CREDITS_NORMAL)
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'Shift' || event.key === "Control") {
+        setCredits(CREDITS_EASTERGG)
+      }
+    };
+
+    const handleKeyUp = (event) => {
+      if (event.key === 'Shift' || event.key === "Control") {
+        setCredits(CREDITS_NORMAL)
+      }
+    };
+
+    // Adicionar event listeners
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+
+    // Cleanup function para remover os event listeners
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+
   return (
     <>
-      <CustomHead 
+      <CustomHead
         title={language.getString("PAGES.CREDITS.META_TITLE")}
         description={language.getString("PAGES.CREDITS.META_DESCRIPTION")}
         url={"https://pixelsplace.nemtudo.me/credits"}
@@ -24,34 +102,11 @@ export default function Credits() {
           <div className={styles.credits}>
 
             {
-              [
-
-                {
-                  name: 'Nem Tudo',
-                  id: '612651439701098558',
-                  role: language.getString("PAGES.CREDITS.CHIEF_COW")
-                },
-                {
-                  name: 'Commandbat',
-                  id: '385478022670843904',
-                  role: language.getString("PAGES.CREDITS.CHIEF_DRAGGABLE")
-                },
-                {
-                  name: 'Renato',
-                  id: '427257953503019017',
-                  role: language.getString("PAGES.CREDITS.GOAT")
-                },
-                {
-                  name: 'Kuriel',
-                  id: '354233941550694400',
-                  role: language.getString("PAGES.CREDITS.CHIEF_EVIL_LINUX")
-                }
-
-              ].map((staff) => (
-                  <div className={styles.staff} key={staff.id}>
-                      <h2>{staff.role}</h2>
-                      <Link href={`/user/${staff.id}`}>{staff.name}</Link>
-                  </div>
+              credits.map((staff) => (
+                <div className={styles.staff} key={staff.id}>
+                  <h2>{staff.role}</h2>
+                  <Link href={`/user/${staff.id}`}>{staff.name}</Link>
+                </div>
               ))
             }
 
