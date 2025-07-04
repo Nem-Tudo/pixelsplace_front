76ca3edd7dc1691726e2991ed69e49ee125b36d8
Nem Tudo <gf98289@gmail.com>
2025-07-04 13:30:07 -0300

    feat: global nProgress (improve color later)

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,19 @@ import 'tippy.js/dist/tippy.css';
 import 'tippy.js/animations/scale-extreme.css';
 import 'tippy.js/animations/shift-toward-extreme.css';
 import "@/styles/tippy.css";
+import '../styles/nProgress.css'
 
 import { AuthProvider } from '../context/AuthContext';
 import { LanguageProvider } from '../context/LanguageContext';
 import PopupProvider from "@/context/PopupContext";
+
+
+import { Router } from "next/router"
+import nProgress from 'nprogress'
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeComplete", nProgress.done);
+Router.events.on("routeChangeError", nProgress.done);
 
 export default function App({ Component, pageProps }) {
   return <>
