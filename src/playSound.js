b4619e37ff8cb65b83d76b66997ff27ddb0572ef
Renato <renato.augusto.almeida.santos@gmail.com>
2025-07-05 17:49:42 -0300

    Refactor: No delay on playSound()

--- src/playSound.js
+++ src/playSound.js
@@ -1,3 +1,11 @@
+// Pré-carrega os áudios
+const audios = {
+    'ColorPick.mp3': new Audio(`/sfx/ColorPick.mp3`),
+    'CooldownOverAlert.mp3': new Audio(`/sfx/CooldownOverAlert.mp3`),
+    'Fail.mp3': new Audio(`/sfx/Fail.mp3`),
+    'PixelPlace.mp3': new Audio(`/sfx/PixelPlace.mp3`),
+}
+
 /**
  * Toca um som para o usuário
  * @param {string} sound - Nome do áudio dentro da pasta de efeitos sonoros (@/public/sfx) 
@@ -6,23 +14,7 @@
  * @param {boolean} [settings.bypassPreference=false] - Se o som deve ser forçado mesmo caso o usuário tenha desabilitado nas preferências 
  */
 export default function playSound(sound, settings = { extension: "mp3", bypassPreference: false }) {
-    if (!settings.bypassPreference) if (localStorage.getItem("preferences.sound_effects_disabled") == "true") return;
-
-    const audio = new Audio(`/sfx/${sound}.${settings.extension}`);
-    audio.play()
-}
-
-/*
-const audios = {
-    'ColorPick.mp3': new Audio(`/sfx/ColorPick.mp3`),
-    'CooldownOverAlert.mp3': new Audio(`/sfx/CooldownOverAlert.mp3`),
-    'Fail.mp3': new Audio(`/sfx/Fail.mp3`),
-    'PixelPlace.mp3': new Audio(`/sfx/PixelPlace.mp3`),
-}
-
-export default function playSound(sound, settings = { extension: "mp3", bypassPreference: false }) {
     if (!settings.bypassPreference && localStorage.getItem("preferences.sound_effects_disabled") == "true") return;
 
     audios[`${sound}.${settings.extension}`].play()
 }
-*/
