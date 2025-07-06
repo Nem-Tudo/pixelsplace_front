4e8f56a62e423f04016be3f4685f42e974c91a8f
Nem Tudo <gf98289@gmail.com>
2025-07-06 00:47:45 -0300

    fix: faction page bugs

--- pages/faction/[factionid].jsx
+++ pages/faction/[factionid].jsx
@@ -26,8 +26,8 @@ export async function getServerSideProps({ req, query }) {
     return match ? decodeURIComponent(match[2]) : undefined;
   }
 
-try {
-    const res = await fetch(`${settings.apiURL}/factions/${query.factionid}`, {
+  try {
+    const res = await fetch(`${settings.apiURL}/factions/${query.factionid}?parseOwner=true`, {
       headers: {
         'Content-Type': 'application/json',
         Authorization: getCookie("authorization"),
@@ -46,14 +46,13 @@ try {
 }
 
 export default function Faction({ faction: factionobject, error, errormessage }) {
-  const { loggedUser, loading, token } = useAuth();
+  const { token } = useAuth();
   const { language } = useLanguage();
   const { openPopup } = usePopup();
-  const [loadingFetch, setLoadingFetch] = useState(false);
 
+  const [loadingFetch, setLoadingFetch] = useState(false);
   const [faction, setFaction] = useState(factionobject);
   const [members, setMembers] = useState([]);
-  const [users, setUsers] = useState({});
 
   useEffect(() => {
     if (faction?.id) {
@@ -62,38 +61,14 @@ export default function Faction({ faction: factionobject, error, errormessage })
   }, [faction?.id]);
 
   async function loadFactionMembers(factionId) {
+    return;
     try {
-      const res = await fetch(`https://apipixelsplace.nemtudo.me/factions/${factionId}/members`);
+      const res = await fetch(`https://apipixelsplace.nemtudo.me/factions/${factionId}/members?parseUser=true`);
       const data = await res.json();
       setMembers(data);
-
-      const userData = {};
-      await Promise.all(
-        data.map(async (member) => {
-          const user = await getUser(member.userId);
-          if (user) {
-            userData[member.userId] = user;
-          }
-        })
-      );
-
-      setUsers(userData);
     } catch (err) {
       console.error("Erro ao buscar membros:", err);
     }
-  }
-
-  async function getUser(id) {
-    const res = await fetch(`${settings.apiURL}/users/${id}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        authorization: token,
-      },
-    });
-
-    if (res.status === 404) return null;
-    return await res.json();
   }
 
   if (error) {
@@ -101,7 +76,7 @@ export default function Faction({ faction: factionobject, error, errormessage })
       <>
         <MainLayout>
           <div>
-            <h1>Erro ao carregar perfil de servidor!</h1>
+            <h1>Erro ao carregar faction!</h1>
             <span>Mensagem: {errormessage}</span>
           </div>
         </MainLayout>
@@ -109,32 +84,11 @@ export default function Faction({ faction: factionobject, error, errormessage })
     )
   }
 
-  const fetchWithAuth = async (url, method, body) => {
-    try {
-      setLoadingFetch(true);
-      const res = await fetch(`${settings.apiURL}${url}`, {
-        method,
-        headers: {
-          "Content-Type": "application/json",
-          authorization: token,
-        },
-        body: JSON.stringify(body),
-      });
-      const data = await res.json();
-      if (!res.ok) throw new Error(data.message || "Erro na requisição.");
-      return data;
-    } catch (err) {
-      openPopup("error", { message: `${err.message}` });
-    } finally {
-      setLoadingFetch(false);
-    }
-  };
-
   if (!faction) return (
     <>
       <MainLayout>
         <div>
-          <h1>Servidor não encontrado</h1>
+          <h1>Facção não encontrada</h1>
         </div>
       </MainLayout>
     </>
@@ -142,11 +96,11 @@ export default function Faction({ faction: factionobject, error, errormessage })
 
   return (
     <>
-      <CustomHead 
+      <CustomHead
         title={language.getString("PAGES.FACTION.META_TITLE", { factionName: faction.name })}
         description={language.getString("PAGES.FACTION.META_DESCRIPTION", { factionName: faction.name, factionUsersCount: faction?.stats?.usersCount })}
         url={`https://pixelsplace.nemtudo.me/faction/${faction?.id}`}
-        imageUrl={faction?.icon_url} 
+        imageUrl={faction?.icon_url}
       />
       <MainLayout>
         <main className={styles.main}>
@@ -166,62 +120,59 @@ export default function Faction({ faction: factionobject, error, errormessage })
               </div>
 
               <div className={styles.name}>
-                <h1 className={styles.displayName}>{faction?.name} {faction?.public ? '🔓': '🔒'}</h1>
+                <h1 className={styles.displayName}>{faction?.name} {faction?.public ? '🔓' : '🔒'}</h1>
                 <p className={styles.userName}>#{faction?.handle}</p>
               </div>
 
               <CustomButton label={language.getString('COMMON.JOIN')} href={faction.invite} />
             </div>
-          <div style={{display: "flex", flexDirection: "column",}}>
+            <div style={{ display: "flex", flexDirection: "column", }}>
 
-            <div className={styles.moreInfo}>
+              <div className={styles.moreInfo}>
 
-              <div className={styles.infoBox}  id={styles.description}>
-                {faction?.description}
-              </div>
-              <div className={styles.infoBox}  id={styles.ownerFac} style={{justifyContent: 'center',}}>
-                {/* {language.getString("PAGES.GUILD.PIXELS_PLACED")}: {faction.stats.pixelsPlacedCount} <br /> */}
-                <div /*fake user */ style={{display: 'flex', flexDirection: 'row'}}>
-                <img src="https://commandbat.com.br/homepage/img/projects/imgproject2.png" alt="" style={{width: '15px', borderRadius: '50%',}}/>
-                {( async () => {
-                  const owner = await getUser(faction?.ownerId);
-                  return <p>{owner?.display_name}</p>;
-                })()}
+                <div className={styles.infoBox} id={styles.description}>
+                  {faction?.description}
+                </div>
+                <div className={styles.infoBox} id={styles.ownerFac} style={{ justifyContent: 'center', }}>
+                  {language.getString("PAGES.GUILD.PIXELS_PLACED")}: {faction.stats.pixelsPlacedCount} <br />
+                  <div style={{ display: 'flex', flexDirection: 'row' }}>
+                    <img src={settings.avatarURL(faction.owner.id, faction.owner.avatar)} alt="" style={{ width: '15px', borderRadius: '50%', }} />
+                    <p>{faction.owner.username}</p>;
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className={styles.moreInfo} >
-              <div className={styles.infoBox}  id={styles.listUsers} style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start !important',}}>
-                <h1 style={{alignSelf: 'center',}}>Lista De membros   {faction.stats.membersCount}/{faction.stats.membersCount}</h1>
-                <div  style={{display: 'flex', flexDirection: 'column'}}>
-                 {members.map((member) => {
-          const user = users[member.userId];
-          return (
-            <div key={member.id} style={{ display: 'flex', alignItems: 'center', gap: '5px', marginBottom: '10px' }}>
-              <img
-                src={user?.avatar || 'https://commandbat.com.br/homepage/img/projects/imgproject2.png'}
-                alt=""
-                style={{ width: '15px', borderRadius: '50%' }}
-              />
-              <p>{user?.username || `User ${member.userId}`}</p>
-            </div>
-          );
-        })}
+              <div className={styles.moreInfo} >
+                <div className={styles.infoBox} id={styles.listUsers} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start !important', }}>
+                  <h1 style={{ alignSelf: 'center', }}>Lista De membros   {faction.stats.membersCount}/{faction.stats.membersCount}</h1>
+                  <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    {members.map((member) => {
+                      return (
+                        <div key={member.id} style={{ display: 'flex', alignItems: 'center', gap: '5px', marginBottom: '10px' }}>
+                          <img
+                            src={member.user.avatar ? settings.avatarURL(member.user.id, member.user.avatar) : "/assets/avatar.png"}
+                            alt=""
+                            style={{ width: '15px', borderRadius: '50%' }}
+                          />
+                          <p>{member.user.username || `User ${member.userId}`}</p>
+                          <span style={{background: "#679467", padding: "3px 6px", borderRadius: "3px"}}>{member.role}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div className={styles.infoBox} id={styles.previewCanva} style={{ justifyContent: 'center', }}>
+                  <h1 style={{ alignSelf: 'center', }}>{faction.stats.pixelsPlacedCount} Pixels colocados</h1>
+
+                  <PremiumButton
+                    onClick={() => openPopup("not_implemented_yet")}
+                  >
+                    {language.getString("PAGES.USER_PROFILE.VIEW_PIXELS", { displayName: faction?.name })}
+                  </PremiumButton>
                 </div>
               </div>
-                <div className={styles.infoBox}  id={styles.previewCanva} style={{justifyContent: 'center',}}>
-                <h1 style={{alignSelf: 'center',}}>{faction.stats.pixelsPlacedCount} Pixels colocados</h1>
-
-                <PremiumButton
-                  onClick={() => openPopup("not_implemented_yet")}
-                >
-                  {language.getString("PAGES.USER_PROFILE.VIEW_PIXELS", { displayName: faction?.name })}
-                </PremiumButton>
-                </div>
             </div>
           </div>
-        </div>
         </main>
       </MainLayout>
     </>
