cc3da872af6e49bc07dbdabd03f5e8eb61eb4f91
command-bat <tiagolaure@gmail.com>
2025-07-06 15:05:11 -0300

    fiz algumas coisas na pag de fac

--- pages/faction/[factionid].jsx
+++ pages/faction/[factionid].jsx
@@ -18,6 +18,7 @@ import { usePopup } from '@/context/PopupContext';
 import Badges from "@/components/Badges";
 import CustomHead from "@/components/CustomHead";
 import Tippy from "@tippyjs/react";
+import Link from "next/link";
 
 export async function getServerSideProps({ req, query }) {
   const cookies = req.headers.cookie || '';
@@ -191,21 +192,32 @@ export default function Faction({ faction: factionobject, error, errormessage })
                   {faction?.description}
                 </div>
                 <div className={styles.infoBox} id={styles.ownerFac} style={{ justifyContent: 'center', }}>
-                  {language.getString("PAGES.GUILD.PIXELS_PLACED")}: {faction.stats.pixelsPlacedCount} <br />
                   <div style={{ display: 'flex', flexDirection: 'row' }}>
+                    <p>Dono:</p>
                     <img src={settings.avatarURL(faction.owner.id, faction.owner.avatar)} alt="" style={{ width: '15px', borderRadius: '50%', }} />
-                    <p>{faction.owner.username}</p>;
+                    <Link href={"/user/"+faction.owner.username}>{faction.owner.username}</Link>
                   </div>
                 </div>
               </div>
 
               <div className={styles.moreInfo} >
+
+                  <div className={styles.infoBox} id={styles.previewCanva} style={{ justifyContent: 'center', }}>
+                  <h1 style={{ alignSelf: 'center', }}>{language.getString("PAGES.GUILD.PIXELS_PLACED")}: {faction.stats.pixelsPlacedCount}</h1>
+
+                  <PremiumButton
+                    onClick={() => openPopup("not_implemented_yet")}
+                  >
+                    {language.getString("PAGES.USER_PROFILE.VIEW_PIXELS", { displayName: faction?.name })}
+                  </PremiumButton>
+                </div>
+
                 <div className={styles.infoBox} id={styles.listUsers} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start !important', }}>
                   <h1 style={{ alignSelf: 'center', }}>Lista De membros   {faction.stats.membersCount}/{faction.stats.membersCount}</h1>
                   <div style={{ display: 'flex', flexDirection: 'column' }}>
                     {members.map((member) => {
                       return (
-                        <div key={member.id} style={{ display: 'flex', alignItems: 'center', gap: '5px', marginBottom: '10px' }}>
+                        <div key={member.id} style={{ display: 'flex', alignItems: 'center', gap: '5px', marginBottom: '10px' }} onClick={() => router.push(`/user/${member.user.username}`)}>
                           <img
                             src={member.user.avatar ? settings.avatarURL(member.user.id, member.user.avatar) : "/assets/avatar.png"}
                             alt=""
@@ -217,15 +229,6 @@ export default function Faction({ faction: factionobject, error, errormessage })
                       );
                     })}
                   </div>
-                </div>
-                <div className={styles.infoBox} id={styles.previewCanva} style={{ justifyContent: 'center', }}>
-                  <h1 style={{ alignSelf: 'center', }}>{faction.stats.pixelsPlacedCount} Pixels colocados</h1>
-
-                  <PremiumButton
-                    onClick={() => openPopup("not_implemented_yet")}
-                  >
-                    {language.getString("PAGES.USER_PROFILE.VIEW_PIXELS", { displayName: faction?.name })}
-                  </PremiumButton>
                 </div>
               </div>
             </div>
