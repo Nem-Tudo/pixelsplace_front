8049fefffaa599f1d5e597fd6ffe72257299e726
Nem Tudo <gf98289@gmail.com>
2025-08-08 17:42:44 -0300

    discord activity try fix websocket

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@
 
 module.exports = {
     apiURL: "/api",
-    socketURL: "/api",
+    socketURL: "wss://pixelsplace.nemtudo.me/api",
     avatarURL: (userid, avatar) => `https://cdn.discordapp.com/avatars/${userid}/${avatar}.webp?size=512`,
     guildIconURL: (guildId, icon) => `https://cdn.discordapp.com/icons/${guildId}/${icon}.webp?size=512`,
     branchURL: (activeBranch, pathname = "", search = "") => `https://pixelsplace-front-git-${activeBranch}-nemtudos-projects.vercel.app${pathname}${search}`,
