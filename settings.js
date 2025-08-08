b73e864029dc751b7415433f2cf491a2dceaa8da
Nem Tudo <gf98289@gmail.com>
2025-08-08 17:25:24 -0300

    try: discord activty support

--- settings.js
+++ settings.js
@@ -10,8 +10,8 @@
 
 
 module.exports = {
-    apiURL: "https://apipixelsplace.nemtudo.me",
-    socketURL: "wss://apipixelsplace.nemtudo.me",
+    apiURL: "/api",
+    socketURL: "/api",
     avatarURL: (userid, avatar) => `https://cdn.discordapp.com/avatars/${userid}/${avatar}.webp?size=512`,
     guildIconURL: (guildId, icon) => `https://cdn.discordapp.com/icons/${guildId}/${icon}.webp?size=512`,
     branchURL: (activeBranch, pathname = "", search = "") => `https://pixelsplace-front-git-${activeBranch}-nemtudos-projects.vercel.app${pathname}${search}`,
